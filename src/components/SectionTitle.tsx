dee122ea4234fe8e676409c3f5fc5f7dfa64ce26
Colin Champdavoine <colinchampdavoine@Colins-MacBook-Pro.local>
2025-01-08 13:55:39 +0100

    fixes categories

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -9,7 +9,7 @@ export const SectionTitle = ({ children, id }: Props) => {
   return (
     <div id={id} className="flex flex-row items-center my-4">
       <span className="w-full h-0.5 bg-gray-400 mr-4"></span>
-      <h1 className="dark:text-white text-4xl font-light text-black whitespace-nowrap">
+      <h1 className="dark:text-white text-4xl font-light text-black text-center">
         
         {children}
       </h1>
